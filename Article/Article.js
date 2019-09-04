606ecfc9774df1d7e9b5b01d897f64e757b5e65d
Maaruf Dauda <mkdauda.work@gmail.com>
2019-09-04 13:00:34 +0100

    :sparkles: Create required components for each article
    All necessary 'document.createElement()' made

--- Article/Article.js
+++ Article/Article.js
@@ -112,3 +112,14 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 */
+
+function createComponent(){
+  const articleClass = document.createElement('div');
+
+  const title = document.createElement('h2');
+
+  const date = document.createElement('p');
+
+  const [firstParagraph , secondParagraph, thirdParagraph] = ['p', 'p', 'p']
+    .map(element => document.createElement(element));
+}
